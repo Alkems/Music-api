c8515696fccdab4f1c1c2f804d5cbfa606531e75
Alkems <mynamezalter@gmail.com>
2023-09-21 12:26:46 +0300

    Add artists id get method.

--- index.js
+++ index.js
@@ -3,13 +3,23 @@ const port = 8080
 const swaggerui = require("swagger-ui-express")
 const swaggerDocument = require("./docs/swagger.json")
 
+const artists = [
+    {id:1, name:"Alkem", country:"Estonia"}, 
+    {id:2, name:"Jadeci", country:"USA"},
+    {id:3, name:"Kanye West", country:"USA"},
+    {id:4, name:"Island Boys", country:"USA"},
+]
+
 app.use("/docs",swaggerui.serve,swaggerui.setup(swaggerDocument))
 
 app.get("/artists", (req,res)=>{
-    res.send([
-        {id:1, name:"Alkem", country:"Estonia"}, 
-        {id:2, name:"Jadeci", country:"USA"}, 
-    ])
+    res.send(artists)
+})
+
+app.get("/artists/:id", (req, res) => {
+    if (typeof artists[req.params.id - 1] === "undefined") {
+        return res.status(404).send({error: "Artist not found!"})
+    }
 })
 
 app.listen(port, ()=> {
