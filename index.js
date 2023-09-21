4d19b482931b9738777639defe27e6ccb9402f8a
Alkems <mynamezalter@gmail.com>
2023-09-21 10:36:43 +0300

    Add swagger.json, update index.js

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const app = require("express")()
 const port = 8080
+const swaggerui = require("swagger-ui-express")
+const swaggerDocument = require("./docs/swagger.json")
+
+app.use("/docs",swaggerui.serve,swaggerui.setup(swaggerDocument))
 
 app.listen(port, ()=> {
     console.log(`API up at: http://localhost:${port}`);
