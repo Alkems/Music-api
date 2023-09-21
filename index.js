240d09d3606311eb9a15c22fde208a1936bb24d8
Alkems <mynamezalter@gmail.com>
2023-09-21 10:54:42 +0300

    Add /artists into index.js. [#186088823]

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ const swaggerDocument = require("./docs/swagger.json")
 
 app.use("/docs",swaggerui.serve,swaggerui.setup(swaggerDocument))
 
+app.get("/artists", (req,res)=>{
+    res.send([
+        {id:1, name:"Alkem", country:"Estonia"}, 
+        {id:2, name:"Jadeci", country:"USA"}, 
+    ])
+})
+
 app.listen(port, ()=> {
     console.log(`API up at: http://localhost:${port}`);
 })
