d0f495577bd14b9dbc9ef6ca2f41c0833cdd94fa
Alkems <mynamezalter@gmail.com>
2023-09-26 15:41:40 +0300

    Add data.js, update index.js.

--- index.js
+++ index.js
@@ -2,25 +2,18 @@ const app = require("express")()
 const port = 8080
 const swaggerui = require("swagger-ui-express")
 const swaggerDocument = require("./docs/swagger.json")
-
-const artists = [
-    {id:1, name:"Alkem", country:"Estonia"}, 
-    {id:2, name:"Jadeci", country:"USA"},
-    {id:3, name:"Kanye West", country:"USA"},
-    {id:4, name:"Island Boys", country:"USA"},
-]
+let artists = require("./artists/data")
 
 app.use("/docs",swaggerui.serve,swaggerui.setup(swaggerDocument))
 
 app.get("/artists", (req,res)=>{
-    res.send(artists)
+    res.send(artists.getAll())
 })
 
-app.get("/artists/:id", (req, res) => {
-    if (typeof artists[req.params.id - 1] === "undefined") {
-        return res.status(404).send({error: "Artist not found!"})
-    }
+app.get("/artists/:id", (req,res)=>{
+    res.send(artists.getById(req.params.id))
 })
+
 
 app.listen(port, ()=> {
     console.log(`API up at: http://localhost:${port}`);
