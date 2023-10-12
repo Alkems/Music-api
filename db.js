6c8ff62edda58948176b8321107f3369b85d9c6e
JannKGK <jannkgk@gmail.com>
2023-10-12 17:47:48 +0300

    add database dialect to .env

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(process.env.DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
-    dialect: 'mariadb',
+    dialect: process.env.DB_DIALECT,
     define: {
         timestamps: true
     },
