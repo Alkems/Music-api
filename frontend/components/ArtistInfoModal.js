f40450d7f7fca753ea8de0ddc7b5d27c2d05a58a
JannKGK <jannkgk@gmail.com>
2023-11-29 08:20:41 +0200

    Select list for song id, only show songs that arent already linked

--- frontend/components/ArtistInfoModal.js
+++ frontend/components/ArtistInfoModal.js
@@ -26,11 +26,16 @@ export default {
                     </tr>
                     <tr>
                         <th>Songs</th>
-                        <div v-for="song in songs">
+                        <div v-for="song in artistSongs">
+                            <!-- add delete button for songlink list -->
                             {{song.name}} - {{song.ArtistSong.role}}
                         </div>
                         <div class="col-auto" v-if="isEditing">
-                            <input type="number" v-model="newArtistSong.SongId" placeholder="Song Id">
+                            <!-- add select list for song id -->
+                            <select v-model="newArtistSong.SongId">
+                                <option disabled value="">Select a song</option>
+                                <option v-for="song in linkableSongs" :value="song.id">{{song.name}}</option>
+                            </select>
                             <input type="text" v-model="newArtistSong.role" placeholder="Role">
                             <button type="button" class="btn btn-success mx-2" @click="linkSongToArtist">Add</button>
                         </div>
@@ -75,7 +80,8 @@ export default {
         return{
             isEditing: false,
             modifiedArtist:{},
-            songs: [],
+            artistSongs: [],
+            linkableSongs: [],
             newArtistSong:{}
         }
     },
@@ -103,7 +109,10 @@ export default {
         },
         async fetchSongs() {
             const artist = await (await fetch(this.API_URL + "/artists/"+ this.artistInModal.id)).json();
-            this.songs = artist.Songs;
+            this.artistSongs = artist.Songs;
+            const songs = await (await fetch(this.API_URL + "/songs")).json();
+            //filter so you cant link songs that are already linked.
+            this.linkableSongs = songs.filter(song => !this.artistSongs.some(artistSong => artistSong.id === song.id));
         },
         startEditing(){
             this.modifiedArtist = {...this.artistInModal}
